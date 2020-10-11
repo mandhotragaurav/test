a5f3771f2b7c2f7c2ffa3817f97b7a5875cf7763
Gaurav <gauravmandhotra5@gmail.com>
2020-10-12 01:47:44 +0530

    update code

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import *
+from django.contrib import admin
 # Register your models here.
 
-admin.site.register(ContacUs)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['user', 'contact_number', 'comment']
+
+admin.site.register(ContacUs, ContactAdmin)
